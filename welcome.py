c88dfa9b754c91fa8d18a7c4319eee0b2686b474
User <ug-fca-gig@ug.edu.ec>
2025-04-23 16:33:59 -0500

    se creo la clase person

--- welcome.py
+++ welcome.py
@@ -1,1 +1,10 @@
-print('chao mundo')
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+if __name__ == "__main__":
+    person = Person("John", 36)
+    print(person.name)
+    print(person.age)
+
